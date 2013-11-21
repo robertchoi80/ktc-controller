a94e76531b07ebd65b61e420c9f67bab9ec99de7
sh.chung <jung33@gmail.com>
2013-11-21 16:27:32 +0900

    fix mistake, sorry Robert

--- attributes/recipe.rb
+++ attributes/recipe.rb
@@ -1,21 +1,11 @@
 #
-# Base run list requirements
+# controller run list requirements
 #
-default[:base][:includes] = [] 
+default[:controller][:includes] = [] 
 
 # selinux permissive when on rhelish things
-case platform_family
-when "rhel"
-  default[:base][:includes] << "selinux::permissive"
-  default[:base][:includes] << "yum"
-when "debian"
-  default[:base][:includes] << "apt"
-  if platform == "ubuntu"
-    default[:base][:includes] << "ubuntu"
-  end  
-end
 
-default[:base][:includes].concat %w{
+default[:controller][:includes].concat %w{
  ktc-identity
  ktc-block-storage
  ktc-network
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
-# CookBook:: base
+# CookBook:: controller
 # Recipe:: default
 # Author:: Jesse Nelson <spheromak@gmail.com>
 #
@@ -7,6 +7,6 @@
 
 # include recipes from our run_list attribute
 #
-node[:base][:includes].each do |recipe|
+node[:controller][:includes].each do |recipe|
   include_recipe recipe
 end
