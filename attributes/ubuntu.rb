a2044142865c16325b9ef6696cd98939bb8c5a23
shchung <jung33@gmail.com>
2013-11-21 16:30:27 +0900

    Delete ubuntu.rb
    no need

--- attributes/ubuntu.rb
+++
@@ -1,6 +0,0 @@
-if platform == "ubuntu"
-  include_attribute "ubuntu"
-
-  default[:ubuntu][:archive_url] = "http://ftp.daum.net/ubuntu"
-  default[:ubuntu][:security_url] = "http://ftp.daum.net/ubuntu"
-end
